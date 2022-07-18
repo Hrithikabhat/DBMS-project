5375c8b06c53009f80a271e6668f9bb57bb12592
Hrithikabhat <89293451+Hrithikabhat@users.noreply.github.com>
2022-07-18 20:19:09 +0530

    JavaScript code

---
+++ footer.js
@@ -0,0 +1,7 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = '<div class="footer-content"><img src="/assets/logo.png" class="logo" alt=""></div><p class="footer-title">About Us!</p><p class="info">At HomeDeck, we have been motivated by a desire to better serve our customers for more than 40 years, gracing their homes with the style, sophistication, elegance and warmth that captures the essence of those who live in them. Being a family-owned firm, we understand the value of establishing true lasting relationships with our clients, and we embrace the opportunities we have to create their dream homes. Customer service is the cornerstone of our company, and from concept to completion, our licensed design team will honor your style and functional needs while guiding you through the process and exceeding your every expectation.</p><p class="info">support emails - help@HomeDeck.com, customersupport@homedeck.com</p><p class="info">telephone - 180 78 00 781, 182 76 90 902</p><p class="footer-credit">HomeDeck, Interior that reflects you! </p>';
+}
+
+createFooter();
